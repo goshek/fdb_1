1e54f4b3802ed3ddcdedc4389410725f1cc9b7bf
goshek <gsh6920@naver.com>
2024-09-23 12:48:18 +0900

    202409231248

---
+++ a_dogfoot_enviroment/Hello_SQL.sql
@@ -0,0 +1,55 @@
+show databases;
+-- MySQL Workbench 환경 설정
+
+-- 1. 주석
+-- 주석 단축키: ctrl + /
+
+-- 1-1. 한 줄 주석
+--  # 사용
+#안녕하세요
+#만나서 반갑습니다.
+
+/*
+1-2. 여러 줄 주석
+/ 2개 사이에 별 2개를 사용
+>> 어떤 문장이나 여러 줄을 작성해도 모두 주석으로 인정
+*/
+
+/*
+2. 글자 크기 변경
+ctrl + 마우스 휠 위아래
+Edit >> Preference >> Fonts & Colors
+*/
+
+/*
+3.SQL 명령어 세팅(대문자, 소문자)
+대소문자 구분x
+SQL 명령어는 관례적으로 대문자로 작성하는 것이 일반적
+Edit > Format > UPCASE / lowercase kewords 선택
+*/
+
+/*
+Dark 모드 설정
+
+*/
+
+/*
+6. sql 기본 단축키
+파일 저장: ctrl + s
+한 줄 복사: ctrl + d
+한 줄 삭제: ctrl + l
+실행 단축키
+ㅇ마우스 커서가 위치하는 쿼리문을 실행: ctrl + enter
+ㅇ마우스 드래그 한 영역의 쿼리문만을 실행: ctrl + shift + enter
+
+새 SQL 에디터 열기: ctrl + t
+SQL 정렬: ctrl + b
+*/
+
+
+
+
+
+
+
+
